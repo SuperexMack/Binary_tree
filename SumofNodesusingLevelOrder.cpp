562e0733e0ba0070681646822a81f75efd09ac40
Mohit Sati <146699961+SuperexMack@users.noreply.github.com>
2023-10-26 09:39:43 +0530

    Add files via upload

---
+++ SumofNodesusingLevelOrder.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+
+public:
+    int data;
+    node *left;
+    node *right;
+
+    // Calling an Constructor
+
+    node(int value)
+    {
+
+        this->data = value;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void sumofnodes(node *root)
+{
+
+    int sum = 0;
+
+    if (root == NULL)
+    {
+
+        return;
+    }
+
+    queue<node *> q;
+
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+
+        node *mack = q.front();
+        q.pop();
+
+        if (mack != NULL)
+        {
+
+            
+
+            sum = sum + mack->data;
+
+            if (mack->left)
+            {
+
+                q.push(mack->left);
+            }
+
+            if (mack->right)
+            {
+
+                q.push(mack->right);
+            }
+        }
+
+        else if (!q.empty())
+        {
+
+            q.push(NULL);
+        }
+    }
+
+    
+    cout << sum;
+}
+
+int main()
+{
+
+    node *root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right->left = new node(6);
+    root->right->right = new node(7);
+    root->right->right->right = new node(8);
+
+    sumofnodes(root);
+
+    return 0;
+}
